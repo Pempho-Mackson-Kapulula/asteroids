07ef7f3428cd4e3f6e6fa00e368d63f5a5d7246c
Pempho Mackson Kapulula <pemphomacksonkapulula@gmail.com>
2026-03-04 10:01:21 +0200

    B: add player to screen

--- main.py
+++ main.py
@@ -1,24 +1,36 @@
 import pygame
 from constants import *
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    y = SCREEN_HEIGHT / 2
+    x = SCREEN_WIDTH / 2
+    player = Player(x,y)
 
-    while True:
+
+    running = True
+    while running:
         log_state()
+        screen.fill("black")
         for event in pygame.event.get():
-            screen.fill("black")
             if event.type == pygame.QUIT:
                 return
         
-            
-            pygame.display.flip()
-            pass
+        player.draw(screen)
+        
+        
+        dt = clock.tick(60)/1000  
+        pygame.display.flip()
+        
 
 
 
